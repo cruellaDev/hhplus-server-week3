1841b85d274b99f6ba07ccc51dddaeee4971f6ee
cruellaDev <cruellaDevLee@gmail.com>
2024-07-05 02:21:14 +0900

    * FEATURES : 1.5 프로젝트 세팅 및 구동

---
+++ src/main/java/com/io/hhplus/concert/ConcertApplication.java
@@ -0,0 +1,18 @@
+package com.io.hhplus.concert;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertApplication.class, args);
+	}
+
+	public void run(ApplicationArguments args) throws Exception {
+
+	}
+
+}
