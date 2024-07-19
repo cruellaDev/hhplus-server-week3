70d5b33dc3c05f4c525a52c819ab760b75ac7f3c
cruellaDev <cruellaDevLee@gmail.com>
2024-07-19 10:03:28 +0900

    * TEST : paymentValidatorTest, paymentServiceTest 추가 및 수정

--- src/test/java/com/io/hhplus/concert/domain/reservation/service/PaymentServiceTest.java
+++ src/test/java/com/io/hhplus/concert/domain/reservation/service/PaymentServiceTest.java
@@ -1,7 +1,7 @@
 package com.io.hhplus.concert.domain.reservation.service;
 
 import com.io.hhplus.concert.common.enums.PayMethod;
-import com.io.hhplus.concert.domain.reservation.model.Payment;
+import com.io.hhplus.concert.domain.reservation.service.model.PaymentModel;
 import com.io.hhplus.concert.domain.reservation.repository.PaymentRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -33,44 +33,12 @@ class PaymentServiceTest {
     }
 
     /**
-     * 결제금액과 티켓합계금액과 비교 - 금액 같을 시
-     */
-    @Test
-    void meetsIfSumOfPayAmountEqualsToSumOfTicketsPrice_when_equal() {
-        // given
-        BigDecimal sumOfPayAmount = BigDecimal.valueOf(10000);
-        BigDecimal sumOfTicketPrice = BigDecimal.valueOf(10000);
-
-        // when
-        boolean isValid = paymentService.meetsIfSumOfPayAmountEqualsToSumOfTicketsPrice(sumOfPayAmount, sumOfTicketPrice);
-
-        // then
-        assertThat(isValid).isTrue();
-    }
-
-    /**
-     * 결제금액과 티켓합계금액과 비교 - 금액 다를 시
-     */
-    @Test
-    void meetsIfSumOfPayAmountEqualsToSumOfTicketsPrice_when_not_equal() {
-        // given
-        BigDecimal sumOfPayAmount = BigDecimal.valueOf(10000);
-        BigDecimal sumOfTicketPrice = BigDecimal.valueOf(20000);
-
-        // when
-        boolean isValid = paymentService.meetsIfSumOfPayAmountEqualsToSumOfTicketsPrice(sumOfPayAmount, sumOfTicketPrice);
-
-        // then
-        assertThat(isValid).isFalse();
-    }
-
-    /**
      * 결제 저장
      */
     @Test
     void savePayment() {
         // given
-        Payment payment = Payment.create(
+        PaymentModel paymentModel = PaymentModel.create(
                 1L,
                 1L,
                 PayMethod.POINT,
@@ -78,18 +46,18 @@ class PaymentServiceTest {
                 BigDecimal.valueOf(10000),
                 BigDecimal.ZERO
         );
-        given(paymentRepository.save(any(Payment.class))).willReturn(payment);
+        given(paymentRepository.save(any(PaymentModel.class))).willReturn(paymentModel);
 
         // when
-        Payment result = paymentService.savePayment(payment);
+        PaymentModel result = paymentService.savePayment(paymentModel);
 
         // then
-        assertAll(() -> assertEquals(payment.paymentId(), result.paymentId()),
-                () -> assertEquals(payment.reservationId(), result.reservationId()),
-                () -> assertEquals(payment.payMethod(), result.payMethod()),
-                () -> assertEquals(payment.payAmount(), result.payAmount()),
-                () -> assertEquals(payment.refundableAmount(), result.refundableAmount()),
-                () -> assertEquals(payment.refundAmount(), result.refundAmount()));
+        assertAll(() -> assertEquals(paymentModel.paymentId(), result.paymentId()),
+                () -> assertEquals(paymentModel.reservationId(), result.reservationId()),
+                () -> assertEquals(paymentModel.payMethod(), result.payMethod()),
+                () -> assertEquals(paymentModel.payAmount(), result.payAmount()),
+                () -> assertEquals(paymentModel.refundableAmount(), result.refundableAmount()),
+                () -> assertEquals(paymentModel.refundAmount(), result.refundAmount()));
     }
 
 }
